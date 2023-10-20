7f804cedc7e63fe320ec31d34397c018e7f6a3ef
Kalahazy <brandon_palacios11@hotmail.com>
2023-10-19 19:34:01 -0600

    feat: :sparkles: Exercise 1 Complete ~ Run with node

--- Ejercicio de Lógica de Programación 1/Ejercicio1.js
+++ Ejercicio de Lógica de Programación 1/Ejercicio1.js
@@ -1,30 +1,59 @@
-// Importar el módulo 'readline' de Node.js para interactuar con la línea de comandos
-// Crear una instancia de readline para manejar la entrada y salida
+// Instancia de readline para manejar la entrada y salida
 const readline = require('readline').createInterface({
     input: process.stdin,  // Usar la entrada estándar (teclado)
     output: process.stdout  // Usar la salida estándar (consola)
 });
 
-// Solicitar tres números al usuario
-readline.question('Ingrese el primer número: ', (num1) => {
-    readline.question('Ingrese el segundo número: ', (num2) => {
-        readline.question('Ingrese el tercer número: ', (num3) => {
+// Solicitar tres números al usuario encapsulando la lógica del proceso del ingreso de cada número para regresar al principio si el usuario ingresa un número no válido
+
+function ValidateNumberOne() {
+    readline.question( 'Ingrese el primer número: ', (num1) => {
+        // Expresión regular para verificar si la entrada "num1" es un número válido
+        if ( /^[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?$/.test(num1) ) {
+            ValidateNumberTwo(num1);
+        } else {
+            console.log(`"${num1}" no es un número válido, por favor, ingrese un número válido.`);
+            console.log("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ");
+            // Llamamos a la primer función para volver a solicitar la entrada
+            ValidateNumberOne();
+        }
+    });
+}
+
+function ValidateNumberTwo(num1) {
+    readline.question( 'Ingrese el segundo número: ', (num2) => {
+        // Expresión regular para verificar si la entrada "num1" es un número válido
+        if ( /^[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?$/.test(num2) ) {
+            ValidateNumberThree(num1, num2);
+        } else {
+            console.log(`"${num2}" no es un número válido, por favor, ingrese un número válido.`);
+            console.log("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ");
+            // Llamamos a la primer función para volver a solicitar la entrada
+            ValidateNumberOne();
+        }
+    });
+}
+
+function ValidateNumberThree(num1, num2) {
+    readline.question( 'Ingrese el segundo número: ', (num3) => {
+        // Expresión regular para verificar si la entrada "num1" es un número válido
+        if ( /^[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?$/.test(num3) ) {
             readline.close();   // Cerrar la interfaz de línea de comandos
             const listNumbers = [parseFloat(num1), parseFloat(num2), parseFloat(num3)];
-
-            // Verificar si los valores ingresados son números válidos
-            if (!isNaN(listNumbers[0]) && !isNaN(listNumbers[1]) && !isNaN(listNumbers[2])) {
+            // Verificar si los números son iguales y mostrar un mensaje
+            if (listNumbers[0] === listNumbers[1] && listNumbers[1] === listNumbers[2]) {
+                console.log('Los números ingresados son iguales.');
+            } else {
                 console.log('Menor a mayor:', listNumbers.sort((a, b) => a - b));
                 console.log('Mayor a menor:', listNumbers.sort((a, b) => b - a));
-
-                // Verificar si los números son iguales y mostrar un mensaje
-                if (listNumbers[0] === listNumbers[1] && listNumbers[1] === listNumbers[2]) {
-                    console.log('Los números ingresados son iguales.');
-                }
-            } else {
-                console.log(listNumbers);
-                console.log('Por favor, ingrese números válidos.');
             }
-        });
+        } else {
+            console.log(`"${num3}" no es un número válido, por favor, ingrese un número válido.`);
+            console.log("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ");
+            // Llamamos a la primer función para volver a solicitar la entrada
+            ValidateNumberOne();
+        }
     });
-});
+}
+
+ValidateNumberOne(); // Iniciar el proceso
